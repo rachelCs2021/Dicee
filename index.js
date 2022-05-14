5c4efb1a80d901d44a7d24d9b7dc8b6e60442a42
rachelCs2021 <rachelchasha@gmail.com>
2022-05-14 23:48:42 +0300

    style changes

--- index.js
+++ index.js
@@ -15,23 +15,25 @@ image1.setAttribute("src", randomImagesSource1)
 
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-let randomDiceImage2 = "dice" + randomNumber2 + ".png"
-
-let randomImagesSource2 = "images/" + randomDiceImage2;
+let randomDiceImage2 = "images/dice" + randomNumber2 + ".png"
 
 let image2 = document.querySelectorAll("img")[1]
 
-image2.setAttribute("src", randomImagesSource2)
+image2.setAttribute("src", randomDiceImage2)
 
 
 // show who win
 
-
-
 if (randomNumber1 > randomNumber2) {
+
     document.querySelector("h1").innerHTML = "Player 1 Win!"
+
 } else if (randomNumber2 > randomNumber1) {
+
     document.querySelector("h1").innerHTML = "Player 2 Win!"
+
 } else {
+
     document.querySelector("h1").innerHTML = "Draw!"
+
 }
